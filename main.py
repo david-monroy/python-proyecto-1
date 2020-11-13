57e490181ff2ff0505012b441b544da54325d2db
david-monroy <djmonroy.17@est.ucab.edu.ve>
2020-11-12 21:39:03 -0400

    :tada: Iniciando proyecto
    Creacion archivo main

---
+++ main.py
@@ -0,0 +1,10 @@
+# Electiva: Programación con Python
+# Proyecto #1 - Pizzería
+
+# Realizado por:
+# David Monroy CI: 26.473.543
+# Carolina Patiño CI:
+
+print('***********************')
+print('*    PIZZERÍA UCAB    *')
+print('***********************')
