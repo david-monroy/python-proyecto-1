a8157f42c17936ad6ea0068eb32f5a9cdefff1a7
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2020-11-19 21:16:25 -0400

    Funcionalidad, no repetir ingredientes

--- choose_ingredients.py
+++ choose_ingredients.py
@@ -13,8 +13,10 @@ def chooseIngredients(ingredients):
         option = input("Ingrese el ingrediente (enter para terminar): ")
         if option != "":
             if option in ingredients.keys():
-                selected_ingredients.append(ingredients[option])
-                print(selected_ingredients) 
+                if ingredients[option] in selected_ingredients:
+                   print("Ya ha agregado", ingredients[option][0], "como ingrediente") 
+                else:
+                    selected_ingredients.append(ingredients[option])
             else:
                 print("¡Debe seleccionar una opción válida!")
         else: 
